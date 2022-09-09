f263522309ec270d8f40b726818730cd42db09ec
Bradley Ross <bradley@cs50.harvard.edu>
2022-09-09 21:26:22 +0000

    Initialize all projects

---
+++ p2/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <title>Project 2</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+
+<body>
+    <h1>Project 2</h1>
+
+    <h2>Mechanics</h2>
+    <ul>
+        <li>...</li>
+        <li>...</li>
+        <li>...</li>
+    </ul>
+
+    <h2>Results</h2>
+    <ul>
+        <li>...</li>
+        <li>...</li>
+        <li>...</li>
+    </ul>
+</body>
+
+</html>
---
+++ practice/README.md
@@ -0,0 +1,4 @@
+# e2 Practice Work
+## By: Bradley Ross
+
+Course work for DGMD E-2
