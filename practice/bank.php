2102aab18b5818bcf5129845f339c6196c14f8a0
Bradley Ross <bradley@cs50.harvard.edu>
2022-09-18 16:30:42 +0000

    e2q8 add half dollar to bank

--- practice/bank.php
+++ practice/bank.php
@@ -6,6 +6,8 @@ $penny_value = .01;
 $nickel_value = .05;
 $dime_value = .10;
 $quarter_value = .25;
+# $0.50 piece added for q8
+$half_dollars_value = .5;
 
 # Define 4 more variables, which will each
 # represent how many of each coin is in the bank
@@ -13,9 +15,10 @@ $pennies = 300;
 $nickels = 5;
 $dimes = 0;
 $quarters = 125;
+$half_dollars = 33;
 
 # Add up how much money is in the piggy bank
-$total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dime_value) + ($quarters * $quarter_value);
+$total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dime_value) + ($quarters * $quarter_value)  + ($half_dollars * $half_dollars_value);;
 
 ?>
 <!DOCTYPE html>
