a9a294eb211d5915c6177ee39c418e289e93559e
Bradley Ross <bradley@cs50.harvard.edu>
2022-09-18 18:13:25 +0000

    w3q10 add warning error to bank

--- practice/bank.php
+++ practice/bank.php
@@ -33,6 +33,8 @@ $total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dim
 </head>
 
 <body>
+    <!-- e2q10 error check -->
+    You have <?php echo $quarter; ?> quarters in your bank.
 
     <img alt='PHPiggy Bank Logo' src='https://s3.amazonaws.com/making-the-internet/php-piggy-bank-logo@2x.png'
         style='width:202px;'>
